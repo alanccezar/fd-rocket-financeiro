d0f8adc5dad8119159279cb08ebfa9105620874b
Antonio <antonio.junior@original.io>
2024-06-22 21:58:09 -0300

    Main Menu Draft

---
+++ frontend-rocket-financeiro/src/pages/index.tsx
@@ -0,0 +1,11 @@
+import MainMenu from "@/components/template/MainMenu";
+
+export default function Home() {
+  return (
+    <main
+      className={`w-screen h-screen`}
+    >
+      <MainMenu />
+    </main>
+  );
+}
